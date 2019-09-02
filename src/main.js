b6def30c7aa4ad797e6c02f6b4cc925060a87845
gaochaochao <chaochao03.gao@pjbest.com>
2019-09-02 10:37:50 +0800

    blog文件

--- src/main.js
+++ src/main.js
@@ -1,20 +1,37 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import  VueResource  from 'vue-resource'
-import iView from 'iView'
-import 'iview/dist/styles/iview.css'
 
-Vue.use(VueResource);
-Vue.use(iView);
+import router from './router'
+import store from './store'
+
+import lodash from 'lodash'
+
+import ElementUI from 'element-ui'
+import '@/assets/theme/index.css'
+
+import '@/assets/icon/iconfont.css'
+
+import {formatTime} from "./utils/time";
+
+
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+
+Object.defineProperty(Vue.prototype, '$_', { value: lodash })
+
+
+Vue.directive('title',  function (el, binding) {
+  document.title = el.dataset.title
+})
+// 格式话时间
+Vue.filter('format', formatTime)
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  template: '<App/>',
+  components: { App }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,104 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'//组件的位置
-import Login from '@/components/Login'
-import Fail from '@/components/Fail'
-import Home from '@/components/home'
-import CustInfo from '@/components/CustInfo'
-import CustList from '@/components/CustList'
-import HomePage from '@/components/HomePage'
-import Index from '@/components/index'
-import Foot from '@/components/foot'
+import Home from '@/Home'
+// import HelloWorld from '@/components/HelloWorld'//组件的位置
+// import Login from '@/components/Login'
+// import Fail from '@/components/Fail'
+// import Home from '@/components/home'
+// import CustInfo from '@/components/CustInfo'
+// import CustList from '@/components/CustList'
+// import HomePage from '@/components/HomePage'
+// import Index from '@/components/index'
+// import Foot from '@/components/foot'
 Vue.use(Router)
 export default new Router({
   routes: [
+    // {
+    //   path: '/',//系统的首页面url
+    //   name: 'Login',
+    //   component: Login//对应上文的import
+    // },{
+    //   path: '/HelloWorld',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },{
+    //   path: '/Fail',
+    //   name: 'Fail',
+    //   component: Fail
+    // },{
+    //   path: '/Home',
+    //   name: 'Home',
+    //   component: Home
+    // },{
+    //   path: '/CustInfo',
+    //   name: 'CustInfo',
+    //   component: CustInfo
+    // },{
+    //   path: '/CustList',
+    //   name: 'CustList',
+    //   component: CustList
+    // },{
+    //   path: '/HomePage',
+    //   name: 'HomePage',
+    //   component: HomePage
+    // },{
+    //   path: '/Index',
+    //   name: 'Index',
+    //   component: Index
+    // },{
+    //   path: '/Foot',
+    //   name: 'Foot',
+    //   component: Foot
+    // },
     {
-      path: '/',//系统的首页面url
-      name: 'Login',
-      component: Login//对应上文的import
-    },{
-      path: '/HelloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },{
-      path: '/Fail',
-      name: 'Fail',
-      component: Fail
-    },{
-      path: '/Home',
+      path: '/write/:id?',
+      component: r => require.ensure([], () => r(require('@/views/blog/BlogWrite')), 'blogwrite'),
+      meta: {
+        requireLogin: true
+      },
+    },
+    {
+      path: '',
       name: 'Home',
-      component: Home
-    },{
-      path: '/CustInfo',
-      name: 'CustInfo',
-      component: CustInfo
-    },{
-      path: '/CustList',
-      name: 'CustList',
-      component: CustList
-    },{
-      path: '/HomePage',
-      name: 'HomePage',
-      component: HomePage
-    },{
-      path: '/Index',
-      name: 'Index',
-      component: Index
-    },{
-      path: '/Foot',
-      name: 'Foot',
-      component: Foot
+      component: Home,
+      children: [
+        {
+          path: '/',
+          component: r => require.ensure([], () => r(require('@/views/Index')), 'index')
+        },
+        {
+          path: '/log',
+          component: r => require.ensure([], () => r(require('@/views/Log')), 'log')
+        },
+        {
+          path: '/archives/:year?/:month?',
+          component: r => require.ensure([], () => r(require('@/views/blog/BlogArchive')), 'archives')
+        },
+        {
+          path: '/messageBoard',
+          component: r => require.ensure([], () => r(require('@/views/MessageBoard')), 'messageboard')
+        },
+        {
+          path: '/view/:id',
+          component: r => require.ensure([], () => r(require('@/views/blog/BlogView')), 'blogview')
+        },
+        {
+          path: '/:type/all',
+          component: r => require.ensure([], () => r(require('@/views/blog/BlogAllCategoryTag')), 'blogallcategorytag')
+        },
+        {
+          path: '/:type/:id',
+          component: r => require.ensure([], () => r(require('@/views/blog/BlogCategoryTag')), 'blogcategorytag')
+        }
+      ]
+    },
+    {
+      path: '/login',
+      component: r => require.ensure([], () => r(require('@/views/Login')), 'login')
+    },
+    {
+      path: '/register',
+      component: r => require.ensure([], () => r(require('@/views/Register')), 'register')
     }
   ]
 })
